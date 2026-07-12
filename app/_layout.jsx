7636f508e5a1a0f2fed7a30ccbc3466c0a0fbd32
Hansi4002 <pabodahansani131@gmail.com>
2026-07-12 05:39:50 +0530

    Add SpeciesProvider to app layout

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,18 +1,21 @@
 import { Stack } from 'expo-router';
 import { AuthProvider } from '../context/AuthContext';
 import { TrailProvider } from '../context/TrailContext';
+import { SpeciesProvider } from '../context/SpeciesContext';
 
 export default function RootLayout() {
   return (
     <AuthProvider>
       <TrailProvider>
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="(auth)" />
-          <Stack.Screen name="(tabs)" />
-          <Stack.Screen name="trail/add" />
-          <Stack.Screen name="trail/[id]" />
-          <Stack.Screen name="trail/edit/[id]" />
-        </Stack>
+        <SpeciesProvider>
+          <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="(auth)" />
+            <Stack.Screen name="(tabs)" />
+            <Stack.Screen name="trail/add" />
+            <Stack.Screen name="trail/[id]" />
+            <Stack.Screen name="trail/edit/[id]" />
+          </Stack>
+        </SpeciesProvider>
       </TrailProvider>
     </AuthProvider>
   );
